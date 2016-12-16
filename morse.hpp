544e8ca816c853e247d744d4d9b12e9bed296f06
Ivan Botero <ivan.botero@protonmail.ch>
2016-12-15 20:15:46 -0500

    Alphabet in Morse - Implemented

--- morse.hpp
+++ morse.hpp
@@ -17,13 +17,46 @@
 #define PAUSE_BETWEEN_WORD      500000      //Miliseconds
 
 /* Alphabet */
-#define ALPHABET_SIZE           32
+#define ALPHABET_SIZE           36
 #define MAX_CHARACTER_LENGTH    5
 
 const int ALPHABET_MORSE[ALPHABET_SIZE][MAX_CHARACTER_LENGTH] = {
     {DOT, DASH}, //A
-    {DASH, DOT, DOT, DOT},//B
-    {DASH,DOT,DASH,DOT}
+    {DASH, DOT, DOT, DOT}, //B
+    {DASH, DOT, DASH, DOT}, //C
+    {DASH, DOT, DOT}, //D
+    {DOT}, //E
+    {DOT, DOT, DASH, DOT}, //F
+    {DASH, DASH, DOT}, //G
+    {DOT, DOT, DOT, DOT}, //H
+    {DOT, DOT}, //I
+    {DOT, DASH, DASH, DASH}, //J
+    {DASH, DOT, DASH}, //K
+    {DOT, DASH, DOT, DOT}, //L
+    {DASH, DASH}, //M
+    {DASH, DOT}, //N
+    {DASH, DASH, DASH}, //O
+    {DOT, DASH, DASH, DOT}, //P
+    {DASH, DASH, DOT, DASH}, //Q
+    {DOT, DASH, DOT}, //R
+    {DOT, DOT, DOT}, //S
+    {DASH}, //T
+    {DOT, DOT, DASH}, //U
+    {DOT, DOT, DOT, DASH}, //V
+    {DOT, DASH, DASH}, //W
+    {DASH, DOT, DOT, DASH}, //X
+    {DASH, DOT, DASH, DASH}, //Y
+    {DASH, DASH, DOT, DOT}, //Z
+    {DOT, DASH, DASH, DASH}, //1
+    {DOT, DOT, DASH, DASH, DASH}, //2
+    {DOT, DOT, DOT, DASH, DASH}, //3
+    {DOT, DOT, DOT, DOT, DASH}, //4
+    {DOT, DOT, DOT, DOT, DOT}, //5
+    {DASH, DOT, DOT, DOT, DOT}, //6
+    {DASH, DASH, DOT, DOT, DOT}, //7
+    {DASH, DASH, DASH, DOT, DOT}, //8
+    {DASH, DASH, DASH, DASH, DOT}, //9
+    {DASH, DASH, DASH, DASH, DASH} //0
 };
 
 #endif /* MORSE_HPP */
